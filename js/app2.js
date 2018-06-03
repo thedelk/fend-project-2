5e62c4251a1f0651f42ebf52d203cf02c62abdcf
Ryan Delk <ryancdelk@gmail.com>
2018-06-03 12:22:21 -0500

    Reorganized and categorized

--- js/app2.js
+++ js/app2.js
@@ -1,19 +1,90 @@
-//  Variables
+///////////////////////////////////////////////////////////////////////////////
+//  MASTER JS
+//
+//
+//
+//  INFORMATION
+//  Project.................Memory Game
+//  Purpose.................Udacity Front-End Nanodegree Program
+//                          Project 2
+//  Creator.................Ryan Delk
+//  Last Change.............June 3, 2018
+//
+//
+//
+//  CONTENTS
+//  1. INITIALIZATION
+//    a. Ready..............Functions to call on $(document).ready
+//    b. Start..............Functions to start the game
+//    c. Reset..............Reset current game
+//
+//  2. SQUARES
+//    a. Matching...........Functions to determine matches
+//    b. Styling............Functions to style the squares upon interaction
+//
+//  3. SCORING
+//    a. Stars..............Functions to decrement stars
+//    b. Win................Declare when game is won
+//
+//
+//
+//  NOTES
+//    To jump to a specific section, search (CTRL + F) for the
+//    section's primary heading number, followed by its
+//    subsection's letter.
+//
+//    e.g. Search for "2b" to go to the Squares/Styling section
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
+
+/*  1a: Ready  ***************************************************************/
+
+// Variables
 const arrIcons = ['burn', 'burn', 'infinity', 'infinity', 'moon', 'moon', 'skull', 'skull', 'code-branch', 'code-branch', 'terminal', 'terminal', 'crow', 'crow', 'seedling', 'seedling'];
-let arrIconsOpen = [];
-let $deck = $('.deck');
-// let $matches = $('.match').length / 2;
-let $moves = $('.moves');
-let $movesLabel = $('.moves-label');
-let $reset = $('.fa-redo');
-let movesCount = 0;
+let $deck = $('.deck'),
+  $moves = $('.moves'),
+  $movesLabel = $('.moves-label'),
+  $reset = $('.fa-redo'),
+  arrIconsOpen = [];
+  movesCount = 0,
+
+// Run after the page has finished loading
+$(document).ready(function () {
+
+  // Call function to create game elements
+  init();
+  $reset.click(resetGameWarning);
+});
 
 
-//////////////////////////////////////////////////////////////////////
-//  COMPLETE
-//////////////////////////////////////////////////////////////////////
 
-//  Create squares
+///////////////////////////////////////////////////////////////////////////////
+//  1b: Start
+///////////////////////////////////////////////////////////////////////////////
+
+// Call functions to enable game functionality, establish global variables
+function init() {
+
+  // Squares need to exist before their classes can be selected
+  createSquares();
+
+  // Variables for selectors that can only be called after squares are created
+  let $square = $('.square');
+
+
+  // Enable functionality for square interaction
+  hoverSquare();
+  $square.click(flipSquares);
+
+  // Properties for "Reset" and "Win" dialog boxes
+  vex.defaultOptions.className = 'vex-theme-default';
+  vex.dialog.buttons.YES.text = 'Yes';
+  vex.dialog.buttons.NO.text = 'No';
+};
+
+// Shuffle the icons, then create li items ($('.square')) for each
 function createSquares() {
   let icons = shuffle(arrIcons);
   for (let icon of icons) {
@@ -21,7 +92,7 @@ function createSquares() {
   }
 };
 
-//  Shuffle icons
+// Function to shuffle array items
 function shuffle(array) {
   var currentIndex = array.length,
     temporaryValue, randomIndex;
@@ -38,61 +109,53 @@ function shuffle(array) {
 
 
 
-//////////////////////////////////////////////////////////////////////
-//  WORKING
-//////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+//  1c: Reset
+///////////////////////////////////////////////////////////////////////////////
 
-function hoverSquare() {
-  $('.square:not(.open)').hover(function () {
-    $(this).toggleClass('square-hover');
-    // $(this).css('transform', 'scale(1.04)');
-  }, function () {
-    $(this).toggleClass('square-hover');
-    // $(this).css('transform', 'scale(1)');
+// Reset game to initialization state
+function resetGame() {
+  $deck.empty();
+  movesCount = -1;
+  moveCounter();
+  $('.far.fa-star').toggleClass('fas far');
+  arrIconsOpen = [];
+  init();
+};
+
+// Modal dialog to confirm reset
+function resetGameWarning() {
+  vex.dialog.confirm({
+    message: `Are you sure you wish to reset?`,
+    callback: function (value) {
+      if (value) {
+        resetGame();
+      }
+    }
   });
 };
 
 
 
-//  Flip squares
-function flipSquares() {
-  let $this = $(this);
-  let $thisClass = $this[0].firstChild.classList[1];
+///////////////////////////////////////////////////////////////////////////////
+//  2a: Matching
+///////////////////////////////////////////////////////////////////////////////
 
-  //  Prevent flipping over more than two cards at once
-  if ($this.hasClass('open') || $this.hasClass('match') || $('.open').length === 2) {
-    return;
-  }
-
-  if (arrIconsOpen.length < 2) {
-    $this.addClass('open show flipInY');
-    arrIconsOpen.push($thisClass);
-  }
-
-  if (arrIconsOpen.length === 2) {
-    checkMatch();
-  }
-};
-
-
-
-//  Check if squares match
+//  Check if the two open squares match
 function checkMatch() {
-
 
   if (arrIconsOpen[0] === arrIconsOpen[1]) {
     $deck.find('.open').removeClass('open show flipInY shake').addClass('match rubberBand');
   } else {
-    $deck.find('.open').addClass('shake');
+    // $deck.find('.open').addClass('shake');
     setTimeout(function () {
-      $deck.find('.open').removeClass('open show flipInY');
-    }, 600);
+      $deck.find('.open').addClass('shake').removeClass('open show flipInY');
+    }, 500);
   }
 
   let $matches = $('.match').length / 2;
 
   if ($matches === 2) {
-    console.log('entered wincon if statement: ' + $matches);
     winGame();
   }
   arrIconsOpen = [];
@@ -101,6 +164,95 @@ function checkMatch() {
 
 
 
+///////////////////////////////////////////////////////////////////////////////
+//  2b: Styling
+///////////////////////////////////////////////////////////////////////////////
+
+// When a square is clicked, do this stuff
+function flipSquares() {
+
+  // Working with currently clicked square
+  let $this = $(this);
+
+  // Traverse DOM element for this square's icon class
+  let $thisClass = $this[0].firstChild.classList[1];
+
+  //  Prevent flipping over more than two cards at once
+  if ($this.hasClass('open') || $this.hasClass('match') || $('.open').length === 2) {
+    return;
+  }
+
+  // If the temporary array doesn't yet have two items in it
+  if (arrIconsOpen.length < 2) {
+    $this.addClass('open show flipInY');
+    arrIconsOpen.push($thisClass); // Add class of clicked square to the temp array
+  }
+
+  // If two squares have been flipped, check to see if they match
+  if (arrIconsOpen.length === 2) {
+    checkMatch();
+  }
+};
+
+// Styling when hovering over a square that hasn't been flipped
+function hoverSquare() {
+  $('.square:not(.open)').hover(function () {
+    $(this).toggleClass('square-hover');
+  }, function () {
+    $(this).toggleClass('square-hover');
+  });
+};
+
+
+///////////////////////////////////////////////////////////////////////////////
+//  3a: Stars
+///////////////////////////////////////////////////////////////////////////////
+
+// Change solid star icon class to outlined version when a star is removed
+function removeStar() {
+  let $star = $('.fas.fa-star:last');
+  $star.toggleClass('fas far');
+};
+
+// Increment moves
+function moveCounter() {
+
+  // Increase move count by one
+  movesCount++;
+
+  // Grammar check so it doesn't read "1 Moves"
+  if (movesCount === 1) {
+    $movesLabel.html('Move');
+  } else {
+    $movesLabel.html('Moves');
+  }
+
+  // Star removal when specific move counts are hit
+  switch (movesCount) {
+    case 15:
+      removeStar();
+      break;
+    case 20:
+      removeStar();
+      break;
+    case 25:
+      removeStar();
+      break;
+    default:
+      break;
+  }
+
+  // Update page display of move count
+  $moves.html(movesCount);
+};
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//  3b: Win
+///////////////////////////////////////////////////////////////////////////////
+
+// Modal dialog to announce that game has been won
 function winGame() {
   let starCount = $('.fas.fa-star').length;
   vex.dialog.confirm({
@@ -115,87 +267,19 @@ function winGame() {
 
 
 
-function removeStar() {
-  let $star = $('.fas.fa-star:last');
-  $star.toggleClass('fas far');
-};
 
 
 
-// Increment moves
-function moveCounter() {
-  movesCount++;
-
-  console.log(movesCount);
-
-  switch (movesCount) {
-    case 1:
-      console.log('Case 1: ' + $movesLabel.html());
-      $movesLabel.html('Move');
-      break;
-    case 15:
-      removeStar();
-      break;
-    case 20:
-      removeStar();
-      break;
-    case 25:
-      removeStar();
-      break;
-    default:
-      $movesLabel.html('Moves');
-  }
-
-  // if (movesCount === 1) {
-  //   $movesLabel.html('Move');
-  // } else {
-  //   $movesLabel.html('Moves');
-  // }
-
-  // if (movesCount === 3 || movesCount === 6 || movesCount === 25) {
-  //   removeStar();
-  // }
-
-  $moves.html(movesCount);
-};
-
-function resetGameWarning() {
-  vex.dialog.confirm({
-    message: `Are you sure you wish to reset?`,
-    callback: function (value) {
-      if (value) {
-        resetGame();
-      }
-    }
-  });
-};
-
-function resetGame() {
-  $deck.empty();
-  movesCount = -1;
-  moveCounter();
-  console.log('resetGame after moveCounter ' + movesCount);
-  $('.far.fa-star').toggleClass('fas far');
-  arrIconsOpen = [];
-  init();
-};
 
 
 
-//  Document ready - initialize
-function init() {
-  createSquares();
 
-  let $square = $('.square');
 
-  hoverSquare();
-  $square.click(flipSquares);
-  vex.defaultOptions.className = 'vex-theme-default';
-  vex.dialog.buttons.YES.text = 'Yes';
-  vex.dialog.buttons.NO.text = 'No';
-};
 
-$(document).ready(function () {
-  init();
-  $reset.click(resetGameWarning);
-});
+
+
+
+
+
+
+//  Variables
