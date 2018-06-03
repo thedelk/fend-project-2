ba5babfe95f9a8800304a9e380e535a6259db3c8
Ryan Delk <ryancdelk@gmail.com>
2018-06-03 15:13:28 -0500

    Removed old app.js, formatted css

--- js/app2.js
+++ js/app2.js
@@ -39,7 +39,9 @@
 
 
 
-/*  1a: Ready  ***************************************************************/
+///////////////////////////////////////
+//  1a. Ready
+///////////////////////////////////////
 
 // Variables
 const arrIcons = ['burn', 'burn', 'infinity', 'infinity', 'moon', 'moon', 'skull', 'skull', 'code-branch', 'code-branch', 'terminal', 'terminal', 'crow', 'crow', 'seedling', 'seedling'];
@@ -47,22 +49,25 @@ let $deck = $('.deck'),
   $moves = $('.moves'),
   $movesLabel = $('.moves-label'),
   $reset = $('.fa-redo'),
-  arrIconsOpen = [];
-  movesCount = 0,
+  arrIconsOpen = [],
+  movesCount = 0;
+
 
 // Run after the page has finished loading
 $(document).ready(function () {
 
   // Call function to create game elements
   init();
+
+  // Reset the game when button is clicked
   $reset.click(resetGameWarning);
 });
 
 
 
-///////////////////////////////////////////////////////////////////////////////
-//  1b: Start
-///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////
+//  1b. Start
+///////////////////////////////////////
 
 // Call functions to enable game functionality, establish global variables
 function init() {
@@ -72,7 +77,6 @@ function init() {
 
   // Variables for selectors that can only be called after squares are created
   let $square = $('.square');
-
 
   // Enable functionality for square interaction
   hoverSquare();
@@ -109,9 +113,9 @@ function shuffle(array) {
 
 
 
-///////////////////////////////////////////////////////////////////////////////
-//  1c: Reset
-///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////
+//  1c. Reset
+///////////////////////////////////////
 
 // Reset game to initialization state
 function resetGame() {
@@ -137,36 +141,38 @@ function resetGameWarning() {
 
 
 
-///////////////////////////////////////////////////////////////////////////////
-//  2a: Matching
-///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////
+//  2a. Matching
+///////////////////////////////////////
 
 //  Check if the two open squares match
 function checkMatch() {
 
+  // If the array elements are the same
   if (arrIconsOpen[0] === arrIconsOpen[1]) {
     $deck.find('.open').removeClass('open show flipInY shake').addClass('match rubberBand');
   } else {
-    // $deck.find('.open').addClass('shake');
     setTimeout(function () {
       $deck.find('.open').addClass('shake').removeClass('open show flipInY');
     }, 500);
   }
 
-  let $matches = $('.match').length / 2;
-
-  if ($matches === 2) {
+  // If all squares have been matched, win the game
+  let $matches = $('.match').length;
+  if ($matches === 16) {
     winGame();
   }
+
+  // Clear temp array, increment move count
   arrIconsOpen = [];
   moveCounter();
 };
 
 
 
-///////////////////////////////////////////////////////////////////////////////
-//  2b: Styling
-///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////
+//  2b. Styling
+///////////////////////////////////////
 
 // When a square is clicked, do this stuff
 function flipSquares() {
@@ -177,7 +183,7 @@ function flipSquares() {
   // Traverse DOM element for this square's icon class
   let $thisClass = $this[0].firstChild.classList[1];
 
-  //  Prevent flipping over more than two cards at once
+  // Prevent flipping over more than two cards at once
   if ($this.hasClass('open') || $this.hasClass('match') || $('.open').length === 2) {
     return;
   }
@@ -185,7 +191,9 @@ function flipSquares() {
   // If the temporary array doesn't yet have two items in it
   if (arrIconsOpen.length < 2) {
     $this.addClass('open show flipInY');
-    arrIconsOpen.push($thisClass); // Add class of clicked square to the temp array
+
+    // Add class of clicked square to the temp array
+    arrIconsOpen.push($thisClass);
   }
 
   // If two squares have been flipped, check to see if they match
@@ -204,9 +212,9 @@ function hoverSquare() {
 };
 
 
-///////////////////////////////////////////////////////////////////////////////
-//  3a: Stars
-///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////
+//  3a. Stars
+///////////////////////////////////////
 
 // Change solid star icon class to outlined version when a star is removed
 function removeStar() {
@@ -220,7 +228,7 @@ function moveCounter() {
   // Increase move count by one
   movesCount++;
 
-  // Grammar check so it doesn't read "1 Moves"
+  // Grammar check so score doesn't read "1 Moves"
   if (movesCount === 1) {
     $movesLabel.html('Move');
   } else {
@@ -248,15 +256,21 @@ function moveCounter() {
 
 
 
-///////////////////////////////////////////////////////////////////////////////
-//  3b: Win
-///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////
+//  3b. Win
+///////////////////////////////////////
 
 // Modal dialog to announce that game has been won
 function winGame() {
+
+  // Get current star count
   let starCount = $('.fas.fa-star').length;
+
+  // Announcement and option to reset
   vex.dialog.confirm({
-    message: `Way to go! You just won the game with ${starCount}/3 star rating. Do you want to play again?`,
+    unsafeMessage: `<div class="text-center"><p>Way to go!</p>` +
+      `<p>You just won the game with ${starCount}/3 star rating.</p>` +
+      `<p>Do you want to play again?</p></div>`,
     callback: function (value) {
       if (value) {
         resetGame();
@@ -264,22 +278,3 @@ function winGame() {
     }
   });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//  Variables
